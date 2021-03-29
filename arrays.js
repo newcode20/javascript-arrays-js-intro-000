f8ad2aee6fb4fc0e9e205ff955539d814e6ac15c
stellar-function-e8f8 <kwooten1993@gmail.com>
2021-03-29 00:48:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -25,6 +25,5 @@ function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
 }
 
 function removeElementFromBeginningOfArray(chocolateBars){
-  chocolateBars.slice(1);
-  return chocolateBars;
+return  chocolateBars.slice(0);
 }
