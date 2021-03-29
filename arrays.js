5465e2db0d540f5ae386466148bfb0db2521c993
stellar-function-e8f8 <kwooten1993@gmail.com>
2021-03-29 00:05:44 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,6 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(chocolateBars, element){
   return [element, ...chocolateBars];
 }
+function destructivelyAddElementToBeginningOfArray(chocolateBars, element){
+  return chocolateBars.unshift(element);
+}
